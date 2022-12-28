017c9aa7b472b5f5556f0471ee9567e54636b1ee
Fatih Şen <fatihydrm@outlook.com>
2022-12-28 22:24:22 +0300

    using socket io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+const socket = io(import.meta.env.VITE_SERVER_URL);
+
 function App() {
+   const [message, setMessage] = useState();
+
+   useEffect(() => {
+      socket.on("receive_message", (data) => {
+         console.log(data);
+      });
+   }, [socket]);
+
+   const submitHandle = (e: any) => {
+      e.preventDefault();
+      socket.emit("send_message", { message: message });
+   };
    return (
-      <div>
-         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio commodi
-         repellendus, illum molestiae praesentium soluta aliquam neque voluptate earum
-         error aliquid minima omnis tempore nam vero laboriosam corporis debitis nemo.
+      <div className='h-full flex justify-center items-center bg-slate-900'>
+         <form
+            className='flex flex-col bg-slate-100 p-3 gap-2 rounded-sm'
+            onSubmit={submitHandle}>
+            <input
+               value={message}
+               className='border w-80 p-1.5'
+               type='text'
+               placeholder='Text...'
+               onChange={(e: any) => setMessage(e.target.value)}
+            />
+            <button className='bg-slate-800 py-1 rounded-sm text-white'>Send</button>
+         </form>
       </div>
    );
 }
---
+++ server/src/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { Server } from "socket.io";
+import cors from "cors";
+import helmet from "helmet";
+const app = express();
+import { createServer } from "http";
+import mongoose from "mongoose";
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(express.static("src/public"));
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const server = createServer(app);
+const socketio = new Server(server);
+
+socketio.on("connection", (socket) => {
+   // console.log(`User connected: ${socket.id}`);
+
+   socket.on("send_message", (message) => {
+      socket.broadcast.emit("receive_message", message);
+   });
+
+   socket.on("disconnect", (socket) => {
+      // console.log(socket);
+   });
+});
+
+server.listen(process.env.port || 5000, () => {
+   console.log(`Server listen ${process.env.port || 5000}`);
+});
+
+(async () => {
+   try {
+      mongoose.set("strictQuery", false);
+      await mongoose.connect(process.env.DB_URL);
+      return console.log("connected db");
+   } catch (err) {
+      return console.log(err);
+   }
+})();
