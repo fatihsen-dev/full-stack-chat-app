5e927de584fca31282fde70aa7c13421c3dcf5e4
Fatih Şen <fatihydrm@outlook.com>
2022-12-28 17:54:57 +0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+   return (
+      <div>
+         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio commodi
+         repellendus, illum molestiae praesentium soluta aliquam neque voluptate earum
+         error aliquid minima omnis tempore nam vero laboriosam corporis debitis nemo.
+      </div>
+   );
+}
+
+export default App;
