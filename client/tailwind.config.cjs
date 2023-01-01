598fd100775c66dd52eeccc1e66c7368a2036b13
Fatih Şen <fatihydrm@outlook.com>
2023-01-01 20:49:15 +0300

    tailwind theme color pallete

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            lightv1: "#ffffff",
+            dark: "#282828",
+            lightv2: "#EEEEEE",
+            gray: "#DDDDDD",
+            blue: "#0C8F8F",
+            green: "#73B06F",
+            yellow: "#FFAD08",
+            NavyBlue: "#405059",
+         },
+      },
    },
    plugins: [],
 };
