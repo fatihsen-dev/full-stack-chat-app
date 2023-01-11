e314529b9fd145fa320a910983b68ca478352ee8
fatihsen-dev <fatihydrm@outlook.com>
2023-01-11 20:05:10 +0300

    update: default name

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,6 +1,12 @@
 import AvatarComponent from "boring-avatars";
 
-export default function Avatar({ name, size }: { name: string; size: number }) {
+export default function Avatar({
+   name = "default",
+   size,
+}: {
+   name: string | undefined;
+   size: number;
+}) {
    return (
       <div className='rounded-full overflow-hidden' style={{ width: size, height: size }}>
          <AvatarComponent name={name} size={size} square={false} variant='beam' />
